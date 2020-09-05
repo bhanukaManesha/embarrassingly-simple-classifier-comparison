455724ec0c3e55893a33ce732c8cc923907adf3f
bhanukaManesha <bhanukamgamage@gmail.com>
2020-09-06 06:47:23 +0800

    add nn module ✨

---
+++ nn.py
@@ -0,0 +1,26 @@
+
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class IndoorNetwork(nn.Module):
+
+    def __init__(self):
+        super(IndoorNetwork, self).__init__()
+        self.fc1 = nn.Linear(in_features=2048, out_features=512)
+        self.fc2 = nn.Linear(in_features=512, out_features=256)
+        self.fc3 = nn.Linear(in_features=256, out_features=128)
+        # self.fc3 = nn.Linear(in_features=512, out_features=256)
+        # self.fc4 = nn.Linear(in_features=256, out_features=128)
+        self.out = nn.Linear(in_features=128, out_features=67)
+
+    def forward(self, x):
+        x = F.leaky_relu(self.fc1(x))
+        x = F.leaky_relu(self.fc2(x))
+        x = F.leaky_relu(self.fc3(x))
+        # x = F.relu(self.fc4(x))
+        return F.softmax(self.out(x), dim=1)
+        # return self.out(x)
+
+
+
