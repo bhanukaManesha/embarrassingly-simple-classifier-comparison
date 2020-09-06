49035650e64d4ee46365954e1b74f3468fbba7aa
bhanukaManesha <bhanukamgamage@gmail.com>
2020-09-06 10:30:58 +0800

    Update nn architecture 🏗

--- nn.py
+++ nn.py
@@ -7,20 +7,19 @@ class IndoorNetwork(nn.Module):
 
     def __init__(self):
         super(IndoorNetwork, self).__init__()
-        self.fc1 = nn.Linear(in_features=2048, out_features=512)
-        self.fc2 = nn.Linear(in_features=512, out_features=256)
-        self.fc3 = nn.Linear(in_features=256, out_features=128)
+        self.fc1 = nn.Linear(in_features=2048, out_features=4096)
+        self.fc2 = nn.Linear(in_features=4096, out_features=2048)
+        # self.fc3 = nn.Linear(in_features=256, out_features=128)
         # self.fc3 = nn.Linear(in_features=512, out_features=256)
         # self.fc4 = nn.Linear(in_features=256, out_features=128)
-        self.out = nn.Linear(in_features=128, out_features=67)
+        self.out = nn.Linear(in_features=2048, out_features=67)
 
     def forward(self, x):
-        x = F.leaky_relu(self.fc1(x))
-        x = F.leaky_relu(self.fc2(x))
-        x = F.leaky_relu(self.fc3(x))
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        # x = F.leaky_relu(self.fc3(x))
         # x = F.relu(self.fc4(x))
         return F.softmax(self.out(x), dim=1)
-        # return self.out(x)
 
 
 
