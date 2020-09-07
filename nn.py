0bcde866a8d3547eca0e0b2481535c97c0e4260f
bhanukaManesha <bhanukamgamage@gmail.com>
2020-09-07 20:42:03 +0800

    Update nn architecture

--- nn.py
+++ nn.py
@@ -8,17 +8,16 @@ class IndoorNetwork(nn.Module):
     def __init__(self):
         super(IndoorNetwork, self).__init__()
         self.fc1 = nn.Linear(in_features=2048, out_features=1024)
+        self.batchnorm1 = nn.BatchNorm1d(1024)
+        self.dropout1 = nn.Dropout(p=0.5)
         self.fc2 = nn.Linear(in_features=1024, out_features=512)
-        self.fc3 = nn.Linear(in_features=512, out_features=256)
-        self.fc4 = nn.Linear(in_features=256, out_features=128)
-        self.out = nn.Linear(in_features=128, out_features=67)
+        self.batchnorm2 = nn.BatchNorm1d(512)
+        self.out = nn.Linear(in_features=512, out_features=67)
 
     def forward(self, x):
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = F.relu(self.fc3(x))
-        x = F.relu(self.fc4(x))
-        return F.softmax(self.out(x), dim=1)
+        x = self.dropout1(F.relu(self.batchnorm1(self.fc1(x))))
+        x = F.relu(self.batchnorm2(self.fc2(x)))
+        return F.log_softmax(self.out(x), dim=1)
 
 
 
