023d06c4ba9991cbfbefe7bb3f7ae8154273e24e
bhanukaManesha <bhanukamgamage@gmail.com>
2020-09-08 08:54:15 +0800

    Add ResNet and Mnasnet networks 🏗

--- nn.py
+++ nn.py
@@ -3,10 +3,10 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-class IndoorNetwork(nn.Module):
+class IndoorResNetNetwork(nn.Module):
 
     def __init__(self):
-        super(IndoorNetwork, self).__init__()
+        super(IndoorResNetNetwork, self).__init__()
         self.fc1 = nn.Linear(in_features=2048, out_features=1024)
         self.batchnorm1 = nn.BatchNorm1d(1024)
         self.dropout1 = nn.Dropout(p=0.5)
@@ -21,3 +21,18 @@ class IndoorNetwork(nn.Module):
 
 
 
+class IndoorMnasnetNetwork(nn.Module):
+
+    def __init__(self):
+        super(IndoorMnasnetNetwork, self).__init__()
+        self.fc1 = nn.Linear(in_features=1280, out_features=1024)
+        self.batchnorm1 = nn.BatchNorm1d(1024)
+        self.dropout1 = nn.Dropout(p=0.5)
+        self.fc2 = nn.Linear(in_features=1024, out_features=512)
+        self.batchnorm2 = nn.BatchNorm1d(512)
+        self.out = nn.Linear(in_features=512, out_features=67)
+
+    def forward(self, x):
+        x = self.dropout1(F.relu(self.batchnorm1(self.fc1(x))))
+        x = F.relu(self.batchnorm2(self.fc2(x)))
+        return F.log_softmax(self.out(x), dim=1)
