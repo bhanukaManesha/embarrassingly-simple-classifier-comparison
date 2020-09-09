8b321dd24c2d88ac2e451738f327e0b943da0736
bhanukaManesha <bhanukamgamage@gmail.com>
2020-09-09 17:22:46 +0800

    Update train all 🎨

--- train_all.py
+++ train_all.py
@@ -3,6 +3,7 @@ import randomforest
 import knn
 import naivebayes
 import svm
+import nn
 
 
 if __name__ == '__main__':
@@ -11,3 +12,4 @@ if __name__ == '__main__':
     knn.run_loop()
     naivebayes.run_loop()
     svm.run_loop()
+    nn.run_loop()
